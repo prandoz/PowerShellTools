836fec3b9a6a35c203a3ae414ca54ab5bd8f3b00
unknown <prandoz@gmail.com>
2022-04-07 22:59:38 +0200

    Create GitStatusLog

---
+++ GitStatusLog.ps1
@@ -0,0 +1,63 @@
+$folderToCheck = ""
+$secondTimeOut = 60
+
+$i = 1
+while ($i -eq 1) {
+	foreach ($file in Get-ChildItem $folderToCheck) {
+		$cleanMessagge = "nothing to commit, working tree clean"
+		cd $file.PSPath
+		git fetch *> $null
+
+		$commit = git status | sls "commits," | Out-String
+
+		if ($commit) {
+			$commit = $commit.ToString().Substring($commit.IndexOf("by") + 3)
+			$commit = $commit.Substring(0, $commit.IndexOf("commits") + 7)
+		}
+		else {
+			$commit = git status | sls "commit," | Out-String
+
+			if ($commit) {
+				$commit = $commit.ToString().Substring($commit.IndexOf("by") + 3)
+				$commit = $commit.Substring(0, $commit.IndexOf("commit") + 6)
+			}
+		}
+
+		if (git status | sls $cleanMessagge) {
+			if ($commit -And -not($commit.Contains("nothing"))) {
+				Write-Host $file "with" $commit "to pull" -fore Green
+			}
+		}
+		else {
+			$add = git status | sls "add"
+			$unstage = git status | sls "unstaged"
+			$push = git status | sls "push"
+
+			if ($commit) {
+				if ($add -Or $unstage) {
+					Write-Host $file "with" $commit "to pull" -fore Red
+				}
+				else {
+					if ($push) {
+						Write-Host $file "with" $commit "to pull" -fore Orange
+					}
+					else {
+						Write-Host $file "with" $commit "to pull" -fore Green
+					}
+				}
+			}
+			else {
+				if ($add -Or $unstage) {
+					Write-Host $file -fore Red
+				}
+				else {
+					if ($push) {
+						Write-Host $file -fore Orange
+					}
+				}
+			}
+		}
+	}
+	Write-Host "#####################################################################################################################################################################" -fore Yellow
+	start-sleep -Seconds $secondTimeOut
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# PowerShell Tools
+It's a collection of tiny powershell tools
+
+#### GitStatusLog
+Edit the script input the folder to be watch containing the different git repositories, and tell the time every when to check
